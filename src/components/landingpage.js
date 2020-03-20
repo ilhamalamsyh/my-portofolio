7835c08b4f84d034049a6d965cef49fe05748461
Ilham Granger <ilhamalamsyah510@gmail.com>
2020-03-20 07:39:11 +0700

    add font-awesome from bootstrap in index.html, add className on Header element in App.js, add Grid & Cell from react-mdl, styling landing page such as: add some texts, icon, avatar, delete service worker in index.js

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,10 +1,52 @@
 import React, { Component } from 'react';
+import {Grid, Cell} from 'react-mdl';
 
 class Landing extends Component{
 	render(){
 		return(
-			<div>
-				<h1>Landing Page</h1>
+			<div style = {{width : '100%', margin : 'auto'}}>
+				<Grid className = "landing-grid">
+					<Cell col={12}>
+						<img 
+						src="https://res.cloudinary.com/dtxq9wbbg/avatars/avatars-v4/v4_avatar-64.png"
+						alt="Avatar"
+						className="avatar-img"
+						/>
+
+						<div className="banner-text">
+							<h1>Full Stack Developer</h1>
+
+							<hr />
+
+							<p>HTML/CSS | Javascript | React | NodeJS | Express | MongoDB | SQL</p>
+
+							<div className="social-link">
+
+								{/* LinkedIn */}
+								<a href="https://www.linkedin.com/in/ilham-alamsyah-94936717a/" target="_blank" rel="noopener noreferrer">
+									<i className="fa fa-linkedin-square" aria-hidden="true" />
+								</a>
+
+								{/* Github */}
+								<a href="https://github.com/ilhamGranger" target="_blank" rel="noopener noreferrer">
+									<i className="fa fa-github-square" aria-hidden="true" />
+								</a>
+
+								{/* Twitter */}
+								<a href="https://twitter.com/ilham_alamsyah9" target="_blank" rel="noopener noreferrer">
+									<i className="fa fa-twitter" aria-hidden="true" />
+								</a>
+
+								{/* Instagram */}
+								<a href="https://www.instagram.com/ilham_alamsyh/?hl=id" target="_blank" rel="noopener noreferrer">
+									<i className="fa fa-instagram" aria-hidden="true" />
+								</a>
+
+							</div>
+						</div>
+					</Cell>
+				</Grid>
+				
 			</div>
 		)
 	}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
 import { BrowserRouter } from 'react-router-dom';
